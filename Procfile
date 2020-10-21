4f2699a860f03488ca20bb1033fd704dfc6e7235
Caroline Carvalho <carvalhocaroline111@gmail.com>
2020-10-20 21:20:02 -0300

    Config Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn feedback.wsgi
--- feedback/settings.py
+++ feedback/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import django_heroku
 
 from pathlib import Path
 from decouple import config
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
