c89ef3dc893555f5be5c3c47db622ee9f5f36e26
Caroline Carvalho <carvalhocaroline111@gmail.com>
2020-10-24 17:01:53 -0300

    set STATICFILES_DIRS

--- feedback/settings.py
+++ feedback/settings.py
@@ -125,8 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static'
 
 DISABLE_COLLECTSTATIC=1
+
+STATICFILES_DIRS = '/staticfiles'
 
 django_heroku.settings(locals())
